da011b05d6bbdc8012bb1479730e6fc5fc2c04a0
CHB94git <christianhb534@gmail.com>
2021-08-11 20:00:46 -0500

    Commit inicial reto5

--- src/main/java/co/edu/utp/misiontic2022/c2/chb/App.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/chb/App.java
@@ -2,10 +2,6 @@ package co.edu.utp.misiontic2022.c2.chb;
 
 import co.edu.utp.misiontic2022.c2.chb.view.ReportesView;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
